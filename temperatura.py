72223d661447b7cc845f5c6783ff9646cc247b6e
CoderGeasy <21170116@uttcampus.edu.mx>
2023-02-27 11:16:39 -0600

    nada

--- temperatura.py
+++ temperatura.py
@@ -1,34 +1,12 @@
-import os
-import glob
-import time
-
-os.system('modprobe w1-gpio')
-os.system('modprobe w1-therm')
-
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
-
-def read_temp_raw():
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
-
-
-def read_temp():
-    lines = read_temp_raw()
-
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-        equals_pos = lines[1].find('t=')
-
-        if equals_pos != -1:
-            temp_string = lines[1][equals_pos+2:]
-            temp_c = float(temp_string) / 1000.0
-            return temp_c
-
-    while True:
-        print("La temp es: "+str(read_temp())+" ˚C")
-        time.sleep(0.5)
+import Adafruit_DHT  
+import time  
+ 
+while True:
+  sensor = Adafruit_DHT.DHT11 #Cambia por DHT22 y si usas dicho sensor
+  pin = 4 #Pin en la raspberry donde conectamos el sensor
+  humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
+ 
+  print ('Humedad: ' , humedad)
+  print ('Temperatura: ' , temperatura)
+  
+  time.sleep(1) #Cada segundo se evalúa el sensor
