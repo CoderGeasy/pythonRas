c8c0807890fa4264aa3ca1f4beecb15e939049a5
CoderGeasy <21170116@uttcampus.edu.mx>
2023-02-27 11:26:31 -0600

    led

--- led.py
+++ led.py
@@ -3,20 +3,20 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(14,GPIO.OUT)
+GPIO.setup(22,GPIO.OUT)
 
 # While loop
 while True:
-        # set GPIO14 pin to HIGH
-        GPIO.output(14,GPIO.HIGH)
+        # set GPIO22 pin to HIGH
+        GPIO.output(22,GPIO.HIGH)
         # show message to Terminal
         print ("LED is ON")
         # pause for one second
         time.sleep(1)
 
 
-        # set GPIO14 pin to HIGH
-        GPIO.output(14,GPIO.LOW)
+        # set GPIO22 pin to HIGH
+        GPIO.output(22,GPIO.LOW)
         # show message to Terminal
         print ("LED is OFF")
         # pause for one second
