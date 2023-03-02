7ea25c3d77b8996cf3764c4968ad25ddfd1af647
CoderGeasy <21170116@uttcampus.edu.mx>
2023-03-02 14:42:39 -0600

    ajustes

--- temperatura.py
+++ temperatura.py
@@ -1,12 +1,37 @@
-import Adafruit_DHT  
-import time  
- 
-while True:
-  sensor = Adafruit_DHT.DHT11 #Cambia por DHT22 y si usas dicho sensor
-  pin = 4 #Pin en la raspberry donde conectamos el sensor
-  humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
- 
-  print ('Humedad: ' , humedad)
-  print ('Temperatura: ' , temperatura)
-  
-  time.sleep(1) #Cada segundo se evalúa el sensor
+import sys
+import time
+import adafruit_dht
+
+# Configuracion del puerto GPIO al cual esta conectado  (GPIO 23)
+pin = 4
+
+# Crea el objeto para acceder al sensor
+# se debe descomentar la linea dependiendo del tipo de sensor (DHT11 o DHT22)
+#sensor = adafruit_dht.DHT11(pin)
+sensor = adafruit_dht.DHT22(pin)
+
+# Funcion principal
+def main():
+    # Ciclo principal infinito
+    while True:
+
+        # Intenta ejecutar las siguientes instrucciones, si falla va a la instruccion except
+        try:
+    		# Obtiene la humedad y la temperatura desde el sensor
+            humedad = sensor.humidity
+            temperatura = sensor.temperature
+
+    		# Imprime en la consola las variables temperatura y humedad con un decimal
+            print('Temperatura={0:0.1f} C  Humedad={1:0.1f}%'.format(temperatura, humedad))
+
+        # Se ejecuta en caso de que falle alguna instruccion dentro del try
+        except RuntimeError as error:
+            # Imprime en pantalla el error
+            print(error.args[0])
+
+        # Duerme 10 segundos
+        time.sleep(10)
+
+# Llama a la funcion principal
+if __name__ == "__main__":
+    main()
