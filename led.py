8edda4c9664ba01f7a142976ef662b80e060446d
CoderGeasy <21170116@uttcampus.edu.mx>
2023-03-03 12:18:00 -0600

    opcion

--- led.py
+++ led.py
@@ -9,19 +9,19 @@ GPIO.setup(pin_led, GPIO.OUT)
 print("hola")
 
 def onOff():
-    print ("Presiona 1 para encender el LED")
-    print("Presiona 2 para apagarlo")
-    
-    opcion = input("Opcion: ")
-    if opcion == "1":  
-        GPIO.output(pin_led, GPIO.HIGH)
-        time.sleep(1) # Espera 1 segundo
-    elif opcion == "2":
-        GPIO.output(pin_led, GPIO.LOW)
-        time.sleep(1) # Espera 1 segundo
-    else:
-        print("Opcion incorrecta")
-        onOff()
+    opcion = 0 # Definir variable antes del ciclo
+    while opcion == 1 or opcion == 2:
+        print ("Presiona 1 para encender el LED")
+        print("Presiona 2 para apagarlo")
+        opcion = input("Opcion: ")
+        if opcion == "1":  
+            GPIO.output(pin_led, GPIO.HIGH)
+            time.sleep(1) # Espera 1 segundo
+        elif opcion == "2":
+            GPIO.output(pin_led, GPIO.LOW)
+            time.sleep(1) # Espera 1 segundo
+        else:
+            print("Opcion incorrecta")
 
 try:
     print("entró")
