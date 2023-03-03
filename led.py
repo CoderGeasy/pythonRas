b2786d45366b313f37fd79f9e187299f007410ab
CoderGeasy <21170116@uttcampus.edu.mx>
2023-03-03 10:38:51 -0600

    nuevo

--- led.py
+++ led.py
@@ -1,29 +1,30 @@
 import time
-import RPi.GPIO as GPIO  #Importamos el paquete RPi.GPIO y en el código nos refiriremos a el como GPIO
+import RPi.GPIO as GPIO
 
-pin_led = 22  #Variable que contiene el pin(GPIO.BCM) al cual conectamos la señal del LED
+pin_led = 22
 
-GPIO.setmode(GPIO.BCM)   #Establecemos el modo según el cual nos refiriremos a los GPIO de nuestra RPi            
-GPIO.setup(pin_led, GPIO.OUT) #Configuramos el GPIO18 como salida
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin_led, GPIO.OUT)
 
 print("hola")
-#Contenemos el código principal en una estructura try para limpiar los GPIO al terminar o presentarse un error
+
+def onOff():
+    print ("Presiona 1 para encender el LED")
+    print("Presiona 2 para apagarlo")
+    
+    opcion = input("Opcion: ")
+    if opcion == "1":  
+        GPIO.output(pin_led, GPIO.HIGH)
+    elif opcion == "2":
+        GPIO.output(pin_led, GPIO.LOW)
+    else:
+        print("Opcion incorrecta")
+        onOff()
+
 try:
     print("entró")
-    def onOff():
-        print ("Presiona 1 para encender el LED")
-        print("Presiona 2 para apagarlo")
-        
-        opcion = input("Opcion: ")  #Leemos la opcion del usuario
-        if opcion == "1":  
-            GPIO.output( pin_led , GPIO.HIGH )
-        elif opcion == "2":
-            GPIO.output( pin_led , GPIO.LOW )
-        else:
-            print("Opcion incorrecta")
-            onOff()
+    onOff()
 except KeyboardInterrupt:
-    # CTRL+C
     print("\nInterrupcion por teclado")
 except:
     print("Otra interrupcion")
