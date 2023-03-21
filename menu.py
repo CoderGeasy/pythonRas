d87f53c96fddec8f991d796d7c45c8558f15a4ea
Gael Emmanuel Acosta Serrano <21170116@uttcampus.edu.mx>
2023-03-21 11:25:10 -0600

    como estaba

--- menu.py
+++ menu.py
@@ -3,42 +3,27 @@ from led import main as led
 from temperatura import main as temperatura
 from ultrasonico import main as ultrasonico
 
-def clear_console():
-    os.system('cls' if os.name=='nt' else 'clear')
-
-def show_menu():
-    clear_console()
-    print("Menu")
-    print("----------------")
-    print("1. Sensor de temperatura")
-    print("2. Sensor ultrasonico")
-    print("3. LED")
-    print("4. Salir")
-    print("----------------")
-
-def get_option():
-    while True:
-        try:
-            opcion = int(input("Opcion: "))
-            if opcion in range(1,5):
-                break
-            else:
-                print("Opción incorrecta. Elige una opción del menú.")
-        except ValueError:
-            print("Opción incorrecta. Ingresa un número del menú.")
-    return opcion
-
 if __name__ == '__main__':
     i = 0
     while i == 0:
-        show_menu()
-        opcion = get_option()
-        if opcion == 1:
-            temperatura.main()
-        elif opcion == 2:
-            ultrasonico.main()
-        elif opcion == 3:
-            led.main()
-        else:
+        print("Menu")
+        print("----------------")
+        print("1. Sensor de temperatura")
+        print("2. Sensor ultrasonico")
+        print("3. LED")
+        print("4. Salir")
+        opcion = int(input("Opcion: "))
+        if opcion in [1, 2, 3]:
+            if opcion == 1:
+                temperatura.main()
+            elif opcion == 2:
+                ultrasonico.main()
+            elif opcion == 3:
+                led.main()
+        elif opcion == 4:
             print("Saliendo...")
             i = 1
+        else:
+            print("Opción incorrecta")
+            print("Elige una opción")
+            opcion = int(input())
